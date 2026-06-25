6fdcb02d555ba9d901e3eff4ecb896e1d5b32cf5
andreas <a@von-burg.net>
2026-06-25 15:45:44 +0200

    initial working commit

---
+++ src/test/n2n/util/attr/mock/PureEnumMock.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace n2n\util\attr\mock;
+
+enum PureEnumMock {
+	case CASE1;
+	case CASE2;
+}
---
+++ src/test/n2n/util/attr/mock/StringBackedEnumMock.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace n2n\util\attr\mock;
+
+enum StringBackedEnumMock: string {
+	case VALUE1 = 'value-1';
+	case VALUE2 = 'value-2';
+}
