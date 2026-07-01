42ceb4a877cb7e30812f461166a5161ff09e1f90
andreas <a@von-burg.net>
2026-07-01 16:57:31 +0200

    mixed $type allowed as arg

--- src/app/n2n/util/attr/trait/RetrieveTrait.php
+++ src/app/n2n/util/attr/trait/RetrieveTrait.php
@@ -26,7 +26,7 @@ trait RetrieveTrait {
 	 * all Type based req functions are served by {@link BasicReqAndOptTrait}
 	 * all ValueObjectType based req functions are served by {@link ValueObjReqAndOptTrait}
 	 */
-	public function req(mixed $path, ?TypeConstraint $type = null) {
+	public function req(mixed $path, mixed $type = null) {
 		return $this->retrieve($path, $type, true);
 	}
 
@@ -38,7 +38,7 @@ trait RetrieveTrait {
 	 * all Type based opt functions are served by {@link BasicReqAndOptTrait}
 	 * all ValueObjectType based opt functions are served by {@link ValueObjReqAndOptTrait}
 	 */
-	public function opt(mixed $path, ?TypeConstraint $type = null, $defaultValue = null) {
+	public function opt(mixed $path, mixed $type = null, $defaultValue = null) {
 		try {
 			return $this->retrieve($path, $type, false, $defaultValue);
 		} catch (MissingAttributeFieldException $e) {
